3f0c4f7899430d97f18dc7687aac5712a961235a
Ritik Bansal <ritikbansal7272@gmail.com>
2024-04-01 13:29:21 +0530

    Your commit message here

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-=======
-// index.js
-import React from "react";
-import ReactDOM from "react-dom";
-import "./app.css";
-import App from "./App";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
->>>>>>> 93018d834655177567e4fe5666ea776969e1a28c
